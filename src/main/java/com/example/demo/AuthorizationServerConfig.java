9aa975615441664fbbe8bfbd9d12332e48f1a871
Lã Xuân Linh <laxuanlong@gmail.com>
2018-12-15 16:35:31 +0700

    Add JWT

--- src/main/java/com/example/demo/AuthorizationServerConfig.java
+++ src/main/java/com/example/demo/AuthorizationServerConfig.java
@@ -3,15 +3,22 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.token.DefaultToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import sun.security.util.Password;
 
 @Configuration
@@ -30,25 +37,48 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         security.checkTokenAccess("isAuthenticated()");
     }
 
+    @Bean
+    public TokenStore tokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        KeyStoreKeyFactory factory = new KeyStoreKeyFactory(new ClassPathResource("mypass.jks"), "mypass".toCharArray());
+        converter.setKeyPair(factory.getKeyPair("linh"));
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices defaultTokenServices(){
+        DefaultTokenServices services = new DefaultTokenServices();
+        services.setTokenStore(tokenStore());
+        services.setSupportRefreshToken(true);
+        return services;
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("client")
                 .secret(encoder.encode("secret"))
                 .authorizedGrantTypes("password")
-                .scopes("all");
+                .scopes("all")
+		.and()
+                .withClient("client-1234")
+                .secret(encoder.encode("secret"))
+                .authorizedGrantTypes("client_credentials")
+                .scopes("profile");
 
         System.out.println(encoder.encode("secret"));
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore());
-
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        endpoints.tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager);
     }
 }
